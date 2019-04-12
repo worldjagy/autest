edfdfcdcc2dbe52038aedd910d38fff179dd8b12
worldjagy <worldjagy@163.com>
2019-04-12 19:04:24 +0800

    Update tool.js

--- tool.js
+++ tool.js
@@ -1,40 +1,72 @@
+var _ = require('lodash')
 var tool = {};
-load = function (url,v) {
-    var fileName = url.substring(url.lastIndexOf('/')+1);
+var sgs = storages.create("app");
+tool.getUrl = function(path){
+    var baseUrl = sgs.get('baseUrl')||'https://raw.githubusercontent.com/worldjagy/autest/master/';
+    var url = baseUrl+path;
+    return url;
+}
+tool.load = function (path, v) {
+    var fileName = path.substring(path.lastIndexOf('/') + 1);
     var sdPath = files.getSdcardPath()
     var libdir = files.join(sdPath, "/slib/")
-    var path = files.join(libdir, fileName+(v?v:''))
-    files.ensureDir(path)
-    var r = files.exists(path)
+    var lpath = files.join(libdir, fileName + (v ? v : ''))
+    files.ensureDir(lpath)
+    var r = files.exists(lpath)
     if (r) {
-        return path;
+        return lpath;
     }
-    files.listDir(libdir, function(name){
-        if(name.startsWith(fileName)){
-            return files.remove(libdir+'/'+name)
+    files.listDir(libdir, function (name) {
+        if (name.startsWith(fileName)) {
+            return files.remove(libdir + '/' + name)
         }
         return false
     })
-    r = http.get(url);
-    files.writeBytes(path, r.body.bytes())
-    return path;
+    r = http.get(this.getUrl(path));
+    files.writeBytes(lpath, r.body.bytes())
+    return lpath;
 }
-tool.loadDex =function(url,v){
-    loadDex(load(url,v));
+tool.loadDex = function (path, v) {
+    loadDex(this.load(path, v));
 }
-tool.loadJar =function(url,v){
-    loadJar(load(url,v));
+tool.loadJar = function (path, v) {
+    loadJar(this.load(path, v));
 }
-tool.require = function(url,v){
-    return require(load(url,v));
+tool.require = function (path, v) {
+    return require(this.load(path, v));
 }
-tool.execScriptFile = function(url,v){
-    engines.execScriptFile(load(url,v))
+tool.execScriptFile = function (path, v) {
+    engines.execScriptFile(this.load(path, v))
 }
-tool.execScript = function(url){
-    var fileName = url.substring(url.lastIndexOf('/')+1,url.lastIndexOf('.'));
-    r = http.get(url);
+tool.execScript = function (path) {
+    r = http.get(this.getUrl(path));
+    var fileName = path.substring(0,path.lastIndexOf('.'));
     engines.execScript(fileName, r.body.string())
+}
+tool.eval = function (path, v) {
+    try {
+        var scripts = sgs.get('scripts', []);
+        var script = _.find(scripts,{path:path})
+        var cnt = '';
+        if(script){
+            if(script.version == v){
+                cnt = script.content;
+                eval(cnt)
+                return;
+            }
+        }
+        r = http.get(this.getUrl(path));
+        cnt = r.body.string();
+        if(script){
+            _.assignIn(script, {content:cnt,version:v})
+        }else{
+            scripts.push({content:cnt,path:path,version:v})
+        }
+        sgs.put('scripts',scripts);
+        eval(cnt);
+    } catch (e) {
+        log(e)
+    }
 }
 
 module.exports = tool;
